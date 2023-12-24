3d673be9e4ad89bb7118f16219859230a55cef0b
Auke <baconcutter@gmail.com>
2023-12-24 22:18:32 +0100

    day 17

--- 17.dart
+++ 17.dart
@@ -1,70 +1,69 @@
 import 'dart:io';
-import 'dart:math';
 
 void main() {
-  var lines = new File('input.txt').readAsLinesSync();
+  List<List<int>> grid = new File('input.txt')
+      .readAsLinesSync()
+      .map((e) => e.split('').map((e) => int.parse(e)).toList())
+      .toList();
 
-  int heatLoss = 0;
-  List<Point> visited = [Point(0, 0)];
-  Point position = Point(0, 0);
-  Point destination = Point(lines[0].length - 1, lines.length - 1);
+  Set seen = Set();
+  List<(int, int, int, int, int, int)> toBeSeen =
+      List.from([(0, 0, 0, 0, 0, 0)]);
 
-  while (position.distanceTo(destination) != 0) {
-    //consider next two moves and take the one with the least heat loss
-    // visited is not an option
-    // if last two x has same x then also not an option.
-    // if last two y has same y then also not an option.
+  while (toBeSeen.isNotEmpty) {
+    var (heatLoss, x, y, deltaX, deltaY, directionTimes) = toBeSeen.removeAt(0);
 
-    // top
-    var top = Point(position.x, position.y - 1);
-    var right = Point(position.x + 1, position.y);
-    var down = Point(position.x, position.y + 1);
-    var left = Point(position.x - 1, position.y);
+    if (x == grid[0].length - 1 && y == grid.length - 1 && directionTimes >= 4) {
+      print(heatLoss);
+      break;
+    }
+
+    if (seen.contains((x, y, deltaX, deltaY, directionTimes))) {
+      continue;
+    }
+    seen.add((x, y, deltaX, deltaY, directionTimes));
+
+    if (directionTimes != 10 && (deltaX, deltaY) != (0, 0)) {
+      int newX = x + deltaX;
+      int newY = y + deltaY;
+      if (newX >= 0 &&
+          newX < grid[0].length &&
+          newY >= 0 &&
+          newY < grid.length) {
+        toBeSeen.add((
+          heatLoss + grid[newY][newX],
+          newX,
+          newY,
+          deltaX,
+          deltaY,
+          directionTimes + 1
+        ));
+      }
+    }
+
+    if (directionTimes >= 4 || (deltaX, deltaY) == (0, 0)) {
+      for (var (newDeltaX, newDeltaY) in [(0, 1), (1, 0), (0, -1), (-1, 0)]) {
+        if ((newDeltaX, newDeltaY) != (deltaX, deltaY) &&
+            (newDeltaX, newDeltaY) != (-deltaX, -deltaY)) {
+          int newX = x + newDeltaX;
+          int newY = y + newDeltaY;
+          if (newX >= 0 &&
+              newX < grid[0].length &&
+              newY >= 0 &&
+              newY < grid.length) {
+            toBeSeen.add((
+              heatLoss + grid[newY][newX],
+              newX,
+              newY,
+              newDeltaX,
+              newDeltaY,
+              1
+            ));
+          }
+        }
+      }
+    }
+
+    toBeSeen.sort((a, b) => a.$1 - b.$1);
   }
-}
-
-int calculate(Point from, List<Point> visited, List<String> lines, int sum) {
-  if (from.x == lines[0].length - 1 && from.y == lines.length - 1) {
-    return sum;
-  }
-
-  List<Point> nextPoints = [];
-
-  List<Point> lastVisits = [];
-  if (visited.length > 1) {
-    lastVisits = visited.sublist(visited.length - 2);
-  }
-
-  var up = Point(from.x, from.y - 1);
-  if (from.y != 0 &&
-      !visited.contains(up) &&
-      !lastVisits.every((element) => element.x == up.x)) {
-    nextPoints.add(up);
-  }
-
-  var down = Point(from.x, from.y - 1);
-  if (from.y != lines.length - 1 &&
-      !visited.contains(down) &&
-      !lastVisits.every((element) => element.x == down.x)) {
-    nextPoints.add(down);
-  }
-
-  var left = Point(from.x - 1, from.y);
-  if (from.x != 0 &&
-      !visited.contains(left) &&
-      !lastVisits.every((element) => element.y == left.y)) {
-    nextPoints.add(left);
-  }
-
-  var right = Point(from.x + 1, from.y);
-  if (from.x != lines[0].length - 1 &&
-      !visited.contains(right) &&
-      !lastVisits.every((element) => element.y == right.y)) {
-    nextPoints.add(right);
-  }
-
-// add to sum
-  // calculate next
-
-  return sum;
 }
